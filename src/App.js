0545c328dde041f2c0738246403798f242b01e1f
Tadeo Daniel <hector_lectric@hotmail.com>
2020-01-30 12:37:05 -0600

    preuba

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola!!!
+          Hola!!! es la preuba 
         </p>
         <a
           className="App-link"
