b8959121899a67f0cfa41511325115de45f35604
Tadeo Daniel <hector_lectric@hotmail.com>
2020-02-12 08:05:51 -0600

    174693/hector.daniel/lifecycle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react' 
 import './App.css'
 import Name from './components/name'
 import User from './components/user'
+import ButtonState from './components/buttonState'
 
-function App () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Name name='Tadeo'> </Name>
-        <User user='1631113121'></User>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  //Mounting 
+  constructor(props) {
+    super(props) 
+    this.state = {
+      counter: 0
+    } 
+  }
+
+  incrementCounter = () => {
+    this.setState({
+      counter: ++this.state.counter
+    }) 
+  } 
+
+  decrementCounter = () => {
+    this.setState({
+      counter: --this.state.counter
+    }) 
+  } 
+  
+  //Mounting and updating 
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Name name='Tadeo'> </Name>
+          <User user='1631113121'></User>
+          <ButtonState counter={this.state.counter} />
+          <button onClick={this.incrementCounter}> + </button>
+          <button onClick={this.decrementCounter}> - </button>
+        </header>
+      </div>
+    ) 
+  }
 }
 
 export default App
---
+++ src/components/buttonState.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react" 
+
+class ButtonState extends Component {
+  //Mounting 
+  constructor(props) {
+    super(props) 
+    console.log("constructor", "props: ", props) 
+  }
+  //Mounting 
+  componentWillMount() {
+    console.log("componentWillMount fired") 
+  }
+
+  //Mounting 
+  componentDidMount() {
+    console.log("componentDidMount fired") 
+  }
+
+  //Updating 
+  componentWillReceiveProps(nextProps) {
+    console.log("componentWillReceiveProps fired, nextProps:", nextProps) 
+  }
+
+  //Updating 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate fired") 
+    return true 
+  }
+
+  //Updating 
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componentWillUpdate, nextProps:", nextProps, " nextState:", nextState)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate, prevProps:", prevProps, " prevState:", prevState)
+  }
+
+  render() {
+    console.log("render on!") 
+    return (
+      <div>
+        Counter value: {this.props.counter}
+      </div>
+    ) 
+  }
+}
+
+export default ButtonState 
